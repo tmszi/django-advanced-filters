bd1f0ff0b88dd6e447a411e3d8582b2c1f207e89
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-01-30 19:27:56 +0100

    disable tests

--- advanced_filters/tests/conftest.py
+++ advanced_filters/tests/conftest.py
@@ -1,13 +1,13 @@
-import pytest
-from tests.factories import SalesRepFactory
-
-
-@pytest.fixture
-def user(db):
-    return SalesRepFactory()
-
-
-@pytest.fixture()
-def client(client, user):
-    client.force_login(user)
-    return client
+#import pytest
+#from tests.factories import SalesRepFactory
+#
+#
+#@pytest.fixture
+#def user(db):
+#    return SalesRepFactory()
+#
+#
+#@pytest.fixture()
+#def client(client, user):
+#    client.force_login(user)
+#    return client
